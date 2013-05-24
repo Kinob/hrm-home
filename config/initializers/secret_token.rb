21afa19d43cf41c8318f61720a88bbc0c67c32a9
Roman Hromenkov <roman.hromenkov@gmail.com>
2013-05-24 21:03:58 +0300

    Some addons

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hrm::Application.config.secret_token = 'b6eb4f3b2a24ccc58a6ce21a76f3411ce92b8694ca5113237370099948e3e429c1860f1c8deb71f83bb9d392d0b222a0b031546fd4872cd3fbf0be5ef3471056'
